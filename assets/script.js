49bd69240cd2643ba9ccfa384c94fadc05c5ebef
J Mac <jbatcheller123@gmail.com>
2023-11-03 17:05:38 -0600

    getting there

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var characters = {
-  special: " ",
-  numbers: " ",
-  upper: " ",
-  lower: " "
-}
 
+var symbol = '!@#$%^&*()'.split;
+var special = " ";
+var numbers = " ";
+var upper = " ";
+var lower = " ";
 var count = 0;
 
 
@@ -21,67 +20,34 @@ function writePassword() {
 }
 
 
-// Add event listener to generate button
-
-// Still confused on the "writePassword", is that a callback?
-
-generateBtn.addEventListener("click", writePassword);
 
 
 //Main function 
+
+
+generateBtn.addEventListener("click", writePassword);
+
 
 function generatePassword() {
 
   var length = prompt("Choose password length: must be between 8-128 characters");
   var specialChars = confirm('Would you like to include special characters?');
   var upperChars = confirm('Would you like in include uppercase characters?');
-  var lowerChars = confirm('Would you like to include lowercase characters? "y" "n" ');
-  var numbers = confirm('Would you like to include numbers? "y" "n" ');
+  var lowerChars = confirm('Would you like to include lowercase characters?');
+  var numbers = confirm('Would you like to include numbers?');
 
 
-// Special characters  yes or no
+  // Random generators 
 
-if(specialChars === true) {
-  characters.special += functionArray[3];
-  count++;
-} else {
-  return; 
-}
+  var randomLower = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 
-//Uppercase characters yes or no
+  var randomUpper = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 
-if (upperChars === true) {
-  characters.upper += functionArray[1];
-  count++;
-} else {
-  return;
-}
 
-if (lowerChars === true) {
-  characters.lower += functionArray[0];
+  var randomNumber = String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 
-} else {
-  return;
-}
 
-if (numbers === true) {
-  characters.numbers += functionArray[2];
-
-} else {
-  return;
-}
-
-//Random lenght of the password
-
-var passLength = " "; 
-for (var i = 8; (parseInt(length) - count); i++) {
-  var randomNumber = Math.floor(Math.random() * 4); 
-
-  passLength += randomNumber;
-
-  return passLength;
-
-}
+  var randomSpecial = String.fromCharCode[Math.floor(Math.random() * symbol.length)];
 
 
 
@@ -89,38 +55,71 @@ for (var i = 8; (parseInt(length) - count); i++) {
 
 
 
-// Random generators, I think this works?  
+  // Special characters  yes or no
 
-var functionArray = [
-
-  function randomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-
-  },
-
-  function randomUpper() {
-   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-
-  },
-
-  function randomNumber() {
-   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-  },
-
-  function randomSpecial() {
-    var symbol = '!@#$%^&*()';
-    return String.fromCharCode[Math.floor(Math.random() * symbol.length)];
+  if (specialChars === true) {
+    special = randomSpecial;
+    count++;
+  } else {
+    return;
   }
 
-];
+  //Uppercase characters yes or no
+
+  if (upperChars === true) {
+    upper = randomUpper;
+    count++;
+  }
+
+  if (lowerChars === true) {
+    lower = randomLower;
+    count++;
+
+  }
+
+  if (numbers === true) {
+    numbers = randomNumber;
+    count++;
+
+  }
+
+  //Random lenght of the password
+
+  var passLength = " ";
+  for (var i = 8; (parseInt(length) - count); i++) {
+    var randomNumber = Math.floor(Math.random() * 4);
+
+    passLength += randomNumber;
 
 
+    passLength += lower;
+    passLength += numbers;
+    passLength += special;
+    passLength += upper;
+
+    return passLength;
+
+  }
 }
 
 
 
-writePassword();
-generatePassword();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
