327e4427e328e3f60a39e694108f119e528a8b84
J Mac <jbatcheller123@gmail.com>
2023-11-07 11:27:52 -0700

    final commit

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,86 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var symbol = "!@#$%^&*()";
+var numbers = "0123456789";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefhijklmnopqrstuvwxyz";
 
-var symbol = '!@#$%^&*()';
-var special = " ";
-var numbers = " ";
-var upper = " ";
-var lower = " ";
-var count = 0;
 
+
+
+
+
+
+//Main function 
+
+
+
+
+
+function generatePassword() {
+  var password = []
+  var characters = "";
+
+  var length = parseInt(prompt("Choose password length: must be between 8-128 characters"));
+
+  if (length > 128 || length < 8) {
+    alert("wrong")
+    return null;
+  }
+
+
+  var specialChars = confirm('Would you like to include special characters?');
+  var upperChars = confirm('Would you like in include uppercase characters?');
+  var lowerChars = confirm('Would you like to include lowercase characters?');
+  var numbersChars= confirm('Would you like to include numbers?');
+
+
+
+  // Special characters  yes or no
+
+  if (specialChars === true) {
+    characters = characters.concat(symbol);
+
+  }
+
+  //Uppercase characters yes or no
+
+  if (upperChars === true) {
+    characters = characters.concat(upper);
+
+  }
+
+
+  if (lowerChars === true) {
+    characters = characters.concat(lower);
+
+
+  }
+
+  //number yes no
+
+  if (numbersChars === true) {
+    characters = characters.concat(numbers);
+
+
+  }
+  
+
+  var allRandomChars = characters.split("")
+console.log(allRandomChars);
+  //Random length of the password
+
+  for (let i = 0; i < length; i++) {
+    var randomIndex =  Math.floor(Math.random() * allRandomChars.length)
+    var randomChar = allRandomChars[randomIndex]
+
+    password.push(randomChar)
+    
+  }
+ 
+  return password.join("")
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -19,108 +91,4 @@ function writePassword() {
 
 }
 
-
-
-
-//Main function 
-
-
 generateBtn.addEventListener("click", writePassword);
-
-
-function generatePassword() {
-
-  var length = prompt("Choose password length: must be between 8-128 characters");
-  var specialChars = confirm('Would you like to include special characters?');
-  var upperChars = confirm('Would you like in include uppercase characters?');
-  var lowerChars = confirm('Would you like to include lowercase characters?');
-  var numbers = confirm('Would you like to include numbers?');
-
-
-  // Random generators 
-
-  var randomLower = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-
-  var randomUpper = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-
-
-  var randomNumber = String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-
-
-  var randomSpecial = String.fromCharCode[Math.floor(Math.random() * symbol.length)];
-
-
-
-
-
-
-
-  // Special characters  yes or no
-
-  if (specialChars === true) {
-    special = symbol;
-    count++;
-  } if (specialChars === false) {
-    return;
-  }
-
-  //Uppercase characters yes or no
-
-  if (upperChars === true) {
-    upper = randomUpper;
-    count++;
-
-  } if (upperChars === false) {
-    return;
-  }
-
-  if (lowerChars === true) {
-    lower = randomLower;
-    count++;
-
-  } if (lowerChars === false) {
-    return;
-  }
-
-  //number yes no
-
-  if (numbers === true) {
-    numbers = randomNumber;
-    count++;
-
-  } if (numbers === false) {
-    return;
-  }
-   
-  //Random length of the password
-
-  var passLength = " ";
-
-  for (var i = 8; (parseInt(length) + count); i++) {
-    var randomNumber = Math.floor(Math.random() * 6);
-    passLength += randomNumber;
-  
-  
-    passLength += lower;
-    passLength += numbers;
-    passLength += special;
-    passLength += upper;
-
-    return passLength;
-
-  }
-
-  for (var j = 128; (parseInt(length) + count); j--) {
-    var randNum = Math.floor(Math.random() * 6);
-    passLength -= randNum;
-
-    passLength -= lower;
-    passLength -= numbers;
-    passLength -= special;
-    passLength -= upper;
-
-    return passLength;
-
-}
-
-}
